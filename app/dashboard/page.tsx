7008d7ee541be7c7aab084aa822520ee37463f88
Kevin Qu <kevinqu88@gmail.com>
2025-04-18 23:59:07 -0400

    fixed lint

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,4 +1,6 @@
-import { AppSidebar } from "@/components/app-sidebar"
+import { AppSidebar } from '@/components/app-sidebar';
+import { Separator } from '@/components/ui/separator';
+import { SidebarInset, SidebarTrigger, SidebarProvider } from '@/components/ui/sidebar';
 import {
   Breadcrumb,
   BreadcrumbItem,
@@ -6,34 +8,36 @@ import {
   BreadcrumbList,
   BreadcrumbPage,
   BreadcrumbSeparator,
-} from "@/components/ui/breadcrumb"
-import { Separator } from "@/components/ui/separator"
-import {
-  SidebarInset,
-  SidebarProvider,
-  SidebarTrigger,
-} from "@/components/ui/sidebar"
+} from '@/components/ui/breadcrumb';
 
 export default function Page() {
   return (
     <SidebarProvider>
       <AppSidebar />
       <SidebarInset>
-        <header className="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+        <header className={ `
+          flex h-16 shrink-0 items-center gap-2 transition-[width,height]
+          ease-linear
+          group-has-data-[collapsible=icon]/sidebar-wrapper:h-12
+        ` }>
           <div className="flex items-center gap-2 px-4">
             <SidebarTrigger className="-ml-1" />
-            <Separator
-              orientation="vertical"
-              className="mr-2 data-[orientation=vertical]:h-4"
-            />
+            <Separator orientation="vertical" className={ `
+              mr-2
+              data-[orientation=vertical]:h-4
+            ` } />
             <Breadcrumb>
               <BreadcrumbList>
-                <BreadcrumbItem className="hidden md:block">
-                  <BreadcrumbLink href="#">
-                    Building Your Application
-                  </BreadcrumbLink>
+                <BreadcrumbItem className={ `
+                  hidden
+                  md:block
+                ` }>
+                  <BreadcrumbLink href="#">Building Your Application</BreadcrumbLink>
                 </BreadcrumbItem>
-                <BreadcrumbSeparator className="hidden md:block" />
+                <BreadcrumbSeparator className={ `
+                  hidden
+                  md:block
+                ` } />
                 <BreadcrumbItem>
                   <BreadcrumbPage>Data Fetching</BreadcrumbPage>
                 </BreadcrumbItem>
@@ -42,14 +46,20 @@ export default function Page() {
           </div>
         </header>
         <div className="flex flex-1 flex-col gap-4 p-4 pt-0">
-          <div className="grid auto-rows-min gap-4 md:grid-cols-3">
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
-            <div className="bg-muted/50 aspect-video rounded-xl" />
+          <div className={ `
+            grid auto-rows-min gap-4
+            md:grid-cols-3
+          ` }>
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
+            <div className="aspect-video rounded-xl bg-muted/50" />
           </div>
-          <div className="bg-muted/50 min-h-[100vh] flex-1 rounded-xl md:min-h-min" />
+          <div className={ `
+            min-h-[100vh] flex-1 rounded-xl bg-muted/50
+            md:min-h-min
+          ` } />
         </div>
       </SidebarInset>
     </SidebarProvider>
-  )
+  );
 }
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    dynamicIO: true,
+    viewTransition: true
+  },
 };
 
 export default nextConfig;
