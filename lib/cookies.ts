322f80946d3270e3560a8b770bd1fd738960efa0
Kevin Qu <kevinqu88@gmail.com>
2025-04-19 11:43:26 -0400

    prisma

---
+++ lib/cookies.ts
@@ -0,0 +1,30 @@
+import { cookies } from 'next/headers';
+import { RequestCookie, ResponseCookie } from 'next/dist/compiled/@edge-runtime/cookies';
+import { ReadonlyRequestCookies } from 'next/dist/server/web/spec-extension/adapters/request-cookies';
+
+export const COOKIE_PATH_HOME = '/';
+export const COOKIE_MAX_AGE_1_YEAR = 31536000;
+
+export async function getAllCookies(): Promise<RequestCookie[]> {
+  const cookieStore: ReadonlyRequestCookies = await cookies();
+  return cookieStore.getAll();
+}
+
+export async function getCookieByName(name: string): Promise<RequestCookie | undefined> {
+  const cookieStore: ReadonlyRequestCookies = await cookies();
+  return cookieStore.get(name);
+}
+
+export async function setCookie(name: string, value: string, options?: ResponseCookie) {
+  const cookieStore: ReadonlyRequestCookies = await cookies();
+  cookieStore.set(name, value, {
+    path: COOKIE_PATH_HOME,
+    ...options,
+    maxAge: COOKIE_MAX_AGE_1_YEAR,
+  });
+}
+
+export async function deleteCookie(name: string) {
+  const cookieStore: ReadonlyRequestCookies = await cookies();
+  cookieStore.delete(name);
+}
--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,32 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { twMerge } from 'tailwind-merge';
+import { clsx, type ClassValue } from 'clsx';
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
+}
+
+/**
+ * String
+ */
+
+export function isTrue(value: string | undefined | null) {
+  return value?.toLowerCase() === 'true';
+}
+
+export function isFalse(value: string | undefined | null) {
+  return value?.toLowerCase() === 'false';
+}
+
+/**
+ * JSON
+ */
+
+export function safeParseJson<T>(jsonString: string, defaultValue: T = {} as T): T {
+  try {
+    const parsedObject = JSON.parse(jsonString);
+    return parsedObject; // Return the parsed object if successful
+  } catch (error) {
+    console.error('Error parsing JSON:', error);
+    return defaultValue;
+  }
 }
