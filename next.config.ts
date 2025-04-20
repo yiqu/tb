68b48058edc3dddbcbc13de480b9c1b21975c50b
Kevin Qu <kevinqu88@gmail.com>
2025-04-19 22:57:45 -0400

    config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    dynamicIO: true,
+   // dynamicIO: true,
     viewTransition: true
   },
 };
