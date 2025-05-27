c6867c62d6a0a526c56a5a2eeab023ac0773e350
Kevin Qu <kevinqu88@gmail.com>
2025-05-26 22:30:56 -0400

    Simplify middlewear

--- middleware.ts
+++ middleware.ts
@@ -32,32 +32,7 @@ export async function middleware(request: NextRequest) {
       const consentGivenDateCookie = await getCookieByName('consent-given');
       const consentGivenDate: string | undefined = consentGivenDateCookie?.value;
 
-      // consent is given
-      if (consentGivenDate !== undefined) {
-        // check if admin password is in cookies already
-        if (nextPath === '/login') {
-          const isAdminPWCorrect = await getCookieByName(ADMIN_PASSWORD_CORRECT_COOKIE_NAME);
-          const isCorrect: boolean = isAdminPWCorrect?.value === 'true';
-          if (isCorrect) {
-            return NextResponse.redirect(new URL('/admin', request.url));
-          }
-          return NextResponse.next();
-        }
-
-        // redirect to login if admin password is not in cookies
-        if (nextPath === '/admin') {
-          const isAdminPWCorrect = await getCookieByName(ADMIN_PASSWORD_CORRECT_COOKIE_NAME);
-          const isCorrect: boolean = isAdminPWCorrect?.value === 'true';
-          if (!isCorrect) {
-            return NextResponse.redirect(new URL('/login', request.url));
-          }
-          return NextResponse.next();
-        }
-
-        return NextResponse.next();
-        
-      } else {
-        // ask for consent
+      if (consentGivenDate === undefined) {
         return NextResponse.redirect(new URL('/consent', request.url));
       }
     }
@@ -83,7 +58,6 @@ export async function middleware(request: NextRequest) {
     }
 
     return NextResponse.next();
-
   }
 
   return NextResponse.next();
