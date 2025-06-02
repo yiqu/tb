d5d4a50c84b60ef0eea3ddf99a5563ee7b783d6d
Kevin Qu <kevinqu88@gmail.com>
2025-06-01 20:55:04 -0400

    metadata for general

--- app/(base)/account/@content/page.tsx
+++ app/(base)/account/@content/page.tsx
@@ -1,4 +1,11 @@
+import { Metadata } from 'next';
+
 import AccountGeneral from '../_components/AccountGeneral';
+
+export const metadata: Metadata = {
+  title: 'Settings',
+  description: 'Manage your account settings',
+};
 
 export default function AccountContentPage() {
   return <AccountGeneral />;
