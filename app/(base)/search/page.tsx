8782413dace0a0e5c14cfdbd532bb3d55098bda9
Kevin Qu <kevinqu88@gmail.com>
2025-06-04 23:24:06 -0400

    table

--- app/(base)/search/page.tsx
+++ app/(base)/search/page.tsx
@@ -1,7 +1,5 @@
 import { Suspense } from 'react';
 
-import { getAllBills } from '@/server/bills/bills.server';
-
 import SearchTableActionBar from './_components/Searchbar';
 import SearchTableParent from './_components/SearchTableParent';
 import SearchTableSkeleton from './_components/SearchTableSkeleton';
@@ -12,13 +10,11 @@ interface SearchPageProps {
 }
 
 export default function SearchPage({}: SearchPageProps) {
-  const billDuesPromise = getAllBills();
-
   return (
     <div className="flex w-full flex-col items-start justify-start gap-y-3">
       <SearchTableActionBar />
       <Suspense fallback={ <SearchTableSkeleton /> }>
-        <SearchTableParent billDuesPromise={ billDuesPromise } />
+        <SearchTableParent />
       </Suspense>
     </div>
   );
