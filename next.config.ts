f9a25657d771ec487d5299873dcf3e3f8873f1dc
Kevin Qu <kevinqu88@gmail.com>
2025-10-16 20:27:10 -0400

    rever next config

--- next.config.ts
+++ next.config.ts
@@ -3,20 +3,16 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   experimental: {
-    //dynamicIO: true,
     //viewTransition: true,
     useCache: true,
-    //ppr: 'incremental',
-    //clientSegmentCache: true,
-    //turbopackPersistentCachingForBuild: true,
-    // turbopackPersistentCachingForDev: true,
+    clientSegmentCache: true,
     cacheComponents: true,
     //turbopackFileSystemCacheForBuild: true,
     turbopackFileSystemCacheForDev: true,
   },
   reactCompiler: process.env.NODE_ENV === 'production' ? true : false,
   // typedRoutes: true
-  
+
   // 10/7/2025 added this to fix the prisma client initialization error
   outputFileTracingIncludes: {
     '/api/**/*': ['./node_modules/.prisma/client/**/*'],
