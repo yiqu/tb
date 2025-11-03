df632c9226618fa7e63b522f648e10449c6970c8
Kevin Qu <kevinqu88@gmail.com>
2025-11-02 22:36:09 -0500

    bill due fav details work

--- components/sidebar/quick-access-group/FavoritesListSection.tsx
+++ components/sidebar/quick-access-group/FavoritesListSection.tsx
@@ -1,15 +1,22 @@
 'use client';
 
 import Link from 'next/link';
+import { useQuery } from '@tanstack/react-query';
 
+import { Skeleton } from '@/components/ui/skeleton';
+import { getUSDFormatter } from '@/lib/number.utils';
 import Typography from '@/components/typography/Typography';
 import { SidebarMenuSubItem } from '@/components/ui/sidebar';
 import { FavoriteEntity } from '@/models/favorites/favorite.model';
+import DateRelativeDisplay from '@/shared/table/DateRelativeDisplay';
 import EntityDisplayMedia from '@/shared/components/EntityDisplayMedia';
+import { getBillDueByIdQueryOptions } from '@/server/bills/query/bills.query';
 import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card';
 
 import FavoriteItemDetails from './FavoriteItemDetails';
 import SidebarMenuSubButtonFavoritesParentWithActive from '../SidebarMenuSubButtonFavoritesParentWithActive';
+
+const usdFormatter = getUSDFormatter();
 
 export default function FavoritesListSection({ allFavorites }: { allFavorites: FavoriteEntity[] }) {
   return (
@@ -38,5 +45,36 @@ export default function FavoritesListSection({ allFavorites }: { allFavorites: F
 }
 
 function FavoriteItemName({ entity }: { entity: FavoriteEntity }) {
+  const {
+    data: billDue,
+    isLoading: isBillDueLoading,
+    isError: isBillDueError,
+  } = useQuery({
+    ...getBillDueByIdQueryOptions(entity.entityType === 'BILL_DUE' ? (entity.billDueId ?? '') : ''),
+    enabled: entity.entityType === 'BILL_DUE' && !!entity.billDueId,
+  });
+
+  if (entity.entityType === 'SUBSCRIPTION') {
+    return <Typography className="truncate">{ entity.name }</Typography>;
+  }
+
+  if (entity.entityType === 'BILL_DUE') {
+    const dueDate: string | undefined = billDue?.dueDate;
+    const subscriptionName: string | undefined = billDue?.subscription.name;
+    const cost: number | undefined = billDue?.cost ?? billDue?.subscription.cost;
+
+    return (
+      <div className="flex w-full flex-row items-center justify-start gap-x-1 truncate">
+        <Typography className="">{ usdFormatter.format(cost ?? 0) } |</Typography>
+        <Typography className="truncate">{ subscriptionName } |</Typography>
+        { isBillDueLoading ?
+          <Skeleton className="h-5 w-full truncate" />
+        : isBillDueError ?
+          <Typography className="truncate text-red-500">Error loading bill due</Typography>
+        : <DateRelativeDisplay time={ dueDate as any } largest={ 1 } updateInterval={ 120_000 } useShortText={ true } className={ `truncate` } overrideHideSeconds /> }
+      </div>
+    );
+  }
+
   return <Typography className="truncate">{ entity.name }</Typography>;
 }
